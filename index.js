91c0373784d8c1616de9b4eaf2923e288ba12661
Lyudmil Borislavov Mihaylov <131765807+LyudmilMihaylov@users.noreply.github.com>
2025-01-31 10:27:46 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 5000
+const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(5000, () => console.log(`App running on port ${port}`));
+app.listen(3000, () => console.log(`App running on port ${port}`));
